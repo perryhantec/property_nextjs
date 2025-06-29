38a2e70e018cd7d8507893ca58b9d32748a6bfe0
Hong <hong.wong@efaith.com.hk>
2025-06-29 22:54:31 +0800

    commit

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ let connected = false;
 
 const connectDB = async () => {
   mongoose.set('strictQuery', true);
-  mongoose.set('bufferTimeout', 30000);
+  mongoose.set('bufferCommands', false)
 
   // If the database is already connected, don't connect again
   if (connected) {
